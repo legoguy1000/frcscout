05c1838f5c4c3ade0f77d2acdecf8e8c88d9b92a
Alex Resnick <adr8292@gmail.com>
2017-03-06 20:26:42 -0500

    moved secret to ini

--- api/routes/blue_alliance.php
+++ api/routes/blue_alliance.php
@@ -5,11 +5,11 @@ $app->group('/tba', function () use ($app) {
 	$app->post('/webhook', function ($request, $response, $args) {
 		$body = $request->getBody();
 		$formData = $request->getParsedBody();
-		$secret = 'sLwqc^tY5K@HhPMcX@dy#VgP?bsy9fFuHzVzQjCM8LLL?+6jYK';
+		$secret = getIniProp('tba_secret');;
 		$event_key = '';
 		$checkSum = $request->getHeader('HTTP-X-TBA-CHECKSUM')[0];
 		if(isset($formData) && !empty($formData) && isset($checkSum) && $checkSum==sha1($secret.$body))
-		{	
+		{
 			newMessageToQueue('ba_webhook', $formData);
 			//error_log('yes', 0);
 		}
