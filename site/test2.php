b78fda607c7aa12c6c9d0a1ac17a9a554cde8689
Alex Resnick <adr8292@gmail.com>
2017-03-06 19:10:05 -0500

    test

--- site/test2.php
+++ site/test2.php
@@ -1,9 +1,8 @@
 <?php
 include('includes.php');
-	
+
 //initialUserNotificationPreferences('5872ad8963cd2');
 
-$secret = a~E:t:"Lhta,/VdPH''"ke]@+.FXVRG'3q]R}g6+E!m5N2R28B
-$test = sha1($secret.$payload);
+echo getLastMatchByEvent('2017vahay');
 
 ?>
