9767a7108fdf5eb426f37932a5716067a8399d18
Alex Resnick <adr8292@gmail.com>
2017-03-06 19:34:44 -0500

    test

--- site/test2.php
+++ site/test2.php
@@ -1,5 +1,9 @@
 <?php
 include('includes.php');
 
+echo getLastMatchByEvent('2017vahay');
+echo '<br/>';
+echo isLastMatch('78', '2017vahay');
+echo '<br/>';
 echo isLastMatchByMatchKey('2017vahay_qm78');
 ?>
